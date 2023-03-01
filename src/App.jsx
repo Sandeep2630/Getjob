a9600fa1fc91c1d8aeb225a43d62dee59f306e5b
sandeep2630 <sandeepthakur.delta4infotech@gmail.com>
2023-03-02 00:09:59 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import "./main.css";
 import Navbar from "./Component/navbar/Navbar";
 
 function App() {
-  return <div><Navbar/></div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
--- src/Component/navbar/Navbar.jsx
+++ src/Component/navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.scss";
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="container">
         <div className="text">
+          {/* <Link to="/"> */}
           <span className="logo">Getjob</span>
+          {/* </Link> */}
           <span className="dot">.</span>
         </div>
         <div className="links">
@@ -16,6 +19,11 @@ const Navbar = () => {
           <span>Become a Seller</span>
           <button>Join</button>
         </div>
+      </div>
+      <hr />
+      <div className="menu">
+        <span>test1</span>
+        <span>test2</span>
       </div>
     </div>
   );
