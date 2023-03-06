08080e97d90510255e27aa907b96b2d20efa4a9e
sandeep2630 <sandeepthakur.delta4infotech@gmail.com>
2023-03-06 23:56:11 +0530

    added window event listener

--- src/Component/navbar/Navbar.jsx
+++ src/Component/navbar/Navbar.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.scss";
 const Navbar = () => {
+  const [active, setActive] = useState(true);
+
+  const scrollShow = () => {
+    window.screenY > 0 ? setActive(true) : setActive(false);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", scrollShow);
+    return () => {
+      window.addEventListener("scroll", scrollShow);
+    };
+  }, []);
+
   return (
-    <div className="navbar">
+    <div className={active ? "navbar active " : "navbar"}>
       <div className="container">
         <div className="text">
           {/* <Link to="/"> */}
@@ -20,11 +32,16 @@ const Navbar = () => {
           <button>Join</button>
         </div>
       </div>
-      <hr />
-      <div className="menu">
-        <span>test1</span>
-        <span>test2</span>
-      </div>
+
+      {active && (
+        <>
+          <hr />
+          <div className="menu">
+            <span>test1</span>
+            <span>test2</span>
+          </div>
+        </>
+      )}
     </div>
   );
 };
